fb194f30361aedfd12ff1a7fae968a1f2d521604
Gabriela Ramalho <gabrielathecreator@gmail.com>
2024-08-05 18:23:35 -0300

    quebra de linha no terminal

--- models/Pessoa.cs
+++ models/Pessoa.cs
@@ -13,8 +13,11 @@ namespace primeiro_projet_c_sharpe.Models
         public int Ano { get; set; }
 
         public void apresentar()
-        { 
+        {
             Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos. Nasci no ano de {Ano}.");
-            }
+            //Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos. \n Nasci no ano de {Ano}.");
+
+            // {\n} significa quebra que linha no terminal
+        }
     }
 }
--- obj/Debug/net8.0/primeiro-projet-c-sharpe.AssemblyInfo.cs
+++ obj/Debug/net8.0/primeiro-projet-c-sharpe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("primeiro-projet-c-sharpe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d7f1bea7f3bef579fcf38cf3e93ba72eac6c998")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63c21e27c9507fe60ecf0fec85b21511b9257fb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("primeiro-projet-c-sharpe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("primeiro-projet-c-sharpe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
