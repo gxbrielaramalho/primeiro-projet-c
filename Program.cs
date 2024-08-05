63c21e27c9507fe60ecf0fec85b21511b9257fb1
Gabriela Ramalho <gabrielathecreator@gmail.com>
2024-08-05 17:51:08 -0300

    Testando uma nova pessoa

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using primeiro_projet_c_sharpe.Models;
 
-Pessoa p = new Pessoa ();
+Pessoa pessoa1 = new Pessoa ();
+
+pessoa1.Nome= "Gabi";
+pessoa1.Idade= 31;
+pessoa1.Ano= 1993;
+
+pessoa1.apresentar();
--- obj/Debug/net8.0/primeiro-projet-c-sharpe.AssemblyInfo.cs
+++ obj/Debug/net8.0/primeiro-projet-c-sharpe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("primeiro-projet-c-sharpe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86826dcc4f42ebbcc9636d5af51482bb63a6b8e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d7f1bea7f3bef579fcf38cf3e93ba72eac6c998")]
 [assembly: System.Reflection.AssemblyProductAttribute("primeiro-projet-c-sharpe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("primeiro-projet-c-sharpe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
