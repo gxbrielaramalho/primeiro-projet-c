7d7f1bea7f3bef579fcf38cf3e93ba72eac6c998
Gabriela Ramalho <gabrielathecreator@gmail.com>
2024-08-05 16:13:57 -0300

    using

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using primeiro_projet_c_sharpe.Models;
+
+Pessoa p = new Pessoa ();
--- models/Pessoa.cs
+++ models/Pessoa.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace primeiro_projet_c_sharpe.models
+namespace primeiro_projet_c_sharpe.Models
 {
     public class Pessoa
     {
         public string Nome { get; set; }
         public int Idade { get; set; }
 
+        public int Ano { get; set; }
+
         public void apresentar()
         { 
-            Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos.")
+            Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos. Nasci no ano de {Ano}.");
             }
     }
 }
